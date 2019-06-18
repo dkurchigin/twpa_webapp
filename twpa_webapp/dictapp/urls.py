7b3a787cf85ad1bd01f8c9b1bb9947a78792873e
Dkurchigin <kurchigin.dmitry@yandex.ru>
2019-06-18 21:32:47 +0300

    rework rules on updateview

--- twpa_webapp/dictapp/urls.py
+++ twpa_webapp/dictapp/urls.py
@@ -4,8 +4,8 @@ import dictapp.views as dictapp
 app_name = 'dictapp'
 
 urlpatterns = [
-    path('', dictapp.DictListView.as_view(), name='index'),
-    path('<int:pk>/', dictapp.DictListView.as_view(), name='concrete_rule'),
+    # path('', dictapp.DictListUpdateView.as_view(), name='index'),
+    path('<int:pk>/', dictapp.DictListUpdateView.as_view(), name='concrete_rule'),
     # path('<int:pk>/', dictapp.BlocksInCondListView.as_view(), name='concrete_phrase_cond'),
     # path('load_json', dictapp.load_json, name='load_json')
 ]
--- twpa_webapp/dictapp/views.py
+++ twpa_webapp/dictapp/views.py
@@ -1,20 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import DictWithRules
+from django.urls import reverse_lazy
 
 
 class DictListView(ListView):
     model = DictWithRules
     template_name = 'dictapp/rule_list.html'
-
-    # def get_queryset(self):
-    #     qs = super(DictListView, self).get_queryset()
-    #     condition = self.kwargs.get('pk')
-    #     print(condition)
-    #     if condition:
-    #         return qs
-    #     else:
-    #         return
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(DictListView, self).get_context_data(**kwargs)
@@ -28,3 +21,28 @@ class DictListView(ListView):
         context['concrete_rule'] = concrete_rule
         print(context)
         return context
+
+
+class DictListUpdateView(UpdateView):
+    model = DictWithRules
+    template_name = 'dictapp/rule_list.html'
+    fields = ('content',)
+
+    def get_context_data(self, **kwargs):
+        context = super(DictListUpdateView, self).get_context_data(**kwargs)
+        context['title'] = "Состояния с фразами"
+        all_rules = DictWithRules.objects.all()
+        context['all_rules'] = all_rules
+
+        rule_number = self.kwargs.get('pk')
+        if rule_number:
+            concrete_rule = DictWithRules.objects.filter(pk=rule_number).first()
+        else:
+            concrete_rule = DictWithRules.objects.filter(pk=0).first()
+        context['concrete_rule'] = concrete_rule
+        # print(context)
+        return context
+
+    def get_success_url(self):
+        return_to = self.kwargs.get('pk')
+        return reverse_lazy('dictapp:concrete_rule', kwargs={'pk': return_to})
